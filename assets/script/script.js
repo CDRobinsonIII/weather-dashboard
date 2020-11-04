d7907b2aea366fb5861f034af2d064bf39493906
Charles Robinson <the.real.robinsons@gmail.com>
2020-11-04 13:50:15 -0800

    Created city search history list.

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,21 +1,41 @@
+var citySearchHistoryList = [];
 
-
+// Create an add event listener for when user enters a city in the search box.
 $(".add-city").on("click", function(event) {
-    //Do we need the city text field to clear after somebody enters a city? If yes, we'll need this event.preventDefault call.
-    event.preventDefault ();
+
+// The event.preventDefault method in this instance (submit button is clicked) prevents the input from clearing. 
+event.preventDefault ();
+
+// If user enters any characters in the search box, then the code inside is processed.
+if ($(".nameOfCity").val()!=="") {
+    // This console.log to to confirm that the city being entered is being accessed correctly.
     console.log("The typed in city is: "+$(".nameOfCity").val());
 
     // Create an input tag to attach the new city to. To append to the city history list.
     var cityHistoryListInput = $('<input>');
+
+    // Create a var to store the name of the city typed in by the user.
     var addCitytoList = $(".nameOfCity").val();
-    var inputCity = $('<input>').addClass("form-control d-block bg-white").val(addCitytoList).attr({'readonly':'true', 'type':'text'});
+
+    // Add city entered by user to the citySearchHistoryList array. 
+    citySearchHistoryList.push(addCitytoList);
+
+    // Create a var to attach the city name to the input tag variable. 
+    var inputCity = cityHistoryListInput.addClass("form-control d-block bg-white").val(addCitytoList).attr({'readonly':'true', 'type':'text'});
+
+    // Append the city name entered by the user to the city history list form. 
     $(".city-history").append(inputCity);
-    console.log("This is the city I added to the var addCityToList: " + addCitytoList);
+
+    // Test to see if entered city was added to the array. 
+    console.log("This is the city that way added to the city search history list array. " +citySearchHistoryList[0]);
+
+    // Clear the search box of the entered city name. 
+    $(".nameOfCity").val("");
+}
+
+// If user doesn't enter anything before it clicks on the search It exits the function with the else/return so user can input another city.
+else {
+    return;
+}
 
 });
-
-
-
-
-
-// ", "type"="Text", "readonly"="true").addClass("form-control d-block bg-white");
